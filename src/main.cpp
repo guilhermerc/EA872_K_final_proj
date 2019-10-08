a928183e2d31405a05b047309325ee4480858e4c
guilhermerc <guilherme.riciolic@gmail.com>
2019-10-08 15:18:36 -0300

    Modifiquei a função main para mostrar um mapa definido em arquivo ('assets/map.config')

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "element.hpp"
+#include "game.hpp"
 #include "screen.hpp"
 
 using namespace std;
@@ -13,16 +14,7 @@ int main()
     // Maximum height: 44
     // Maximum width: 143
     View::Screen * screen = new View::Screen(44, 143);
-
-    /*
-    Model::Element * player_u = new Model::Element("./assets/sprites/player_u.sprite", 1, 0);
-    Model::Element * player_r = new Model::Element("./assets/sprites/player_r.sprite", 1, 10);
-    Model::Element * player_d = new Model::Element("./assets/sprites/player_d.sprite", 1, 20);
-    Model::Element * player_l = new Model::Element("./assets/sprites/player_l.sprite", 1, 30);
-    Model::Element * ground = new Model::Element("./assets/sprites/ground.sprite", 1, 40);
-    Model::Element * pillar = new Model::Element("./assets/sprites/pillar.sprite", 1, 50);
-    Model::Element * border = new Model::Element("./assets/sprites/border.sprite", 0, 60);
-    */
+    Model::Game * game = new Model::Game("./assets/map.config");
 
     // TODO: Get map dimensions from Map object
     // TODO: Get border dimensions from its Element object
@@ -40,8 +32,10 @@ int main()
     for(int i = 0; i < 91; i++)
         borders[i]->render();
 
+    game->render();
+
     //screen->debug();
-    std::this_thread::sleep_for(std::chrono::milliseconds(30000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(120000));
     screen->stop();
 
     return 0;
